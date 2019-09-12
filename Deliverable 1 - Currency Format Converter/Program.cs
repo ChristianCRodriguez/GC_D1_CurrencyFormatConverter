41fbe1100709e44187e296f60f08f20a157ce58d
Christian Rodriguez <christian@totallifechanges.com>
2019-09-12 11:17:18 -0400

    First Commit to D1

--- Deliverable 1 - Currency Format Converter/Program.cs
+++ Deliverable 1 - Currency Format Converter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,32 @@ namespace Deliverable_1___Currency_Format_Converter
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            List<double> dollarAmounts = new List<double>();
+            const double jpExchangeRate = 107.54, seExchangeRate = 9.68, thExchangeRate = 30.61;
+
+            Console.WriteLine("Hello! Please enter the first dollar amount in the following format: 0.00");
+            dollarAmounts.Add(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Now enter the second dollar amount");
+            dollarAmounts.Add(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Now enter the final dollar amount");
+            dollarAmounts.Add(Convert.ToDouble(Console.ReadLine()));
+
+            Console.WriteLine("Total: {0}", dollarAmounts.Sum());
+            Console.WriteLine("Average Amount: {0}", dollarAmounts.Average());
+            Console.WriteLine("Largest Amount: {0}", dollarAmounts.Max());
+            Console.WriteLine("Smallest Amount: {0}", dollarAmounts.Min());
+
+            var total = dollarAmounts.Sum();
+
+            Console.WriteLine("US Dollars: {0}", total.ToString("C2", CultureInfo.CreateSpecificCulture("en-US")));
+            Console.WriteLine("Swedish Krona: {0}", (total * seExchangeRate).ToString("C2", CultureInfo.CreateSpecificCulture("sv-SE")));
+            Console.WriteLine("Japanese Yen: {0}", (total * jpExchangeRate).ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+            Console.WriteLine("Thai Baht: {0}", (total * thExchangeRate).ToString("C2", CultureInfo.CreateSpecificCulture("th-TH")));
+
+            Console.ReadLine();
         }
     }
 }
